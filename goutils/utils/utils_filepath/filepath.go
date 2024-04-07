3961f7ba13d2fc4d905f48c3ac5d521b09d68fe3
dd84ai <dd84ai@gmail.com>
2024-04-07 18:16:40 +0200

    feat: filepath contains func

--- goutils/utils/utils_filepath/filepath.go
+++ goutils/utils/utils_filepath/filepath.go
@@ -2,6 +2,7 @@ package utils_filepath
 
 import (
 	"path/filepath"
+	"strings"
 
 	"github.com/darklab8/go-utils/goutils/utils"
 	"github.com/darklab8/go-utils/goutils/utils/utils_types"
@@ -17,4 +18,8 @@ func Dir(path utils_types.FilePath) utils_types.FilePath {
 
 func Base(path utils_types.FilePath) utils_types.FilePath {
 	return utils_types.FilePath(filepath.Base(string(path)))
+}
+
+func Contains(path utils_types.FilePath, subpath utils_types.FilePath) bool {
+	return strings.Contains(path.ToString(), subpath.ToString())
 }
